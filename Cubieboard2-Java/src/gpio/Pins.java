98033d86362466e6ee6e2637de1e2797f46e9807
Tiagop <Tiagop@Tiago-PC>
2015-03-20 00:50:48 -0300

    Inserido Array de contantes que guardam as informações do pinos -pinsUSB(completo) -pinsSATA(incompleto)
    Atualizado JavaDoc da biblioteca

--- Cubieboard2-Java/src/gpio/Pins.java
+++ Cubieboard2-Java/src/gpio/Pins.java
@@ -8,20 +8,93 @@ package gpio;
 /**
  *
  * @author Tiagop
+ * 
  * Classe Usada para Armazenar as funcoes dos pinos da CB2
+ * 
+ * A Cubieboard 2 Tem 2 grandes conjuntos de Gpio's:
+ * <ul>
+ * <li>Um do mesmo lado da USB, que nessa classe armazenaremos os dados no Array 'pinsUSB'</li>
+ * <li>e outro que fica do lado oposto da placa,mesmo lado da SATA, ainda irei definir Array para ele</li>
+ * </ul>
  * 
  */
 public class Pins {
     /**
-     * Array guarda as informações ref aos pinos gpio do lado da HDMI
-     */    
-    public String pinsHDMI[][]= {
-        {null,"VCC-5V"},
+     * Array guarda as informações ref aos pinos gpio do lado da USB
+     * obs.:pino 1 (físico) fica prox a entrada LAN lado interno da gpio
+     * pino 2(físico)tambémprox a LAN,mas na borda da placa 
+     */     
+    public final String pinsUSB[][]= {
+        {null,null},
+        {null,"VCC-5V"},//1
         {"11","ph15"},
         {null,"CSI1"},
         {"10","ph15"},
-        {"9" ,"pb19"},
-        {"9" ,"pb19"},
+        {"9" ,"pg0"},
+        {"3" ,"pb18"},
+        {"2" ,"pb19"},
+        {"1" ,"pg3"},
+        {"8" ,"pg2"},
+        {"7" ,"pg1"},
+        {"6" ,"pg4"},//11
+        {"5" ,"pg5"},
+        {"4" ,"pg6"},
+        {"19" ,"pg7"},
+        {"18" ,"pg8"},
+        {"17" ,"pg9"},
+        {"16" ,"pg10"},
+        {"15" ,"pg11"},
+        {null ,"GND"},
+        {null ,"GND"},
+        {null ,"FMINL"},//21
+        {"14" ,"pi4"},
+        {null ,"FMINR"},
+        {"13" ,"pi5"},
+        {null ,"GND"},
+        {"12" ,"pi6"},
+        {null ,"VGA-R"},
+        {"27" ,"pi7"},
+        {null ,"VGA-G"},
+        {"26" ,"pi8"},
+        {null ,"VGA-B"},
+        {"25" ,"pi9"},
+        {null ,"LCD1-VSYNC"},
+        {"24" ,"pe4"},
+        {null ,"LCD1-HSYNC"},
+        {"23" ,"pe5"},
+        {null ,"GND"},
+        {"22" ,"pe6"},
+        {null ,"AVCC"},
+        {"21" ,"pe7"},
+        {null ,"LRADCO"},
+        {"20" ,"pe8"},
+        {null ,"CVBS"},
+        {"35" ,"pe9"},
+        {null ,"HPL"},
+        {"34" ,"pe10"},
+        {null ,"HPR"},
+        {"33" ,"pe11"}
+    };
+    
+    /**
+     * Array guarda as informações ref aos pinos gpio do lado da SATA
+     * obs.: pino 1 (físico)fica prox a entrada SATA lado interno
+     * e pino 2 na borda 
+     */ 
+    public final String pinsSATA[][]={
+       {null,null},
+       {"32","pd0"},
+       {null,"GND"},
+       {"30","pd2"},
+       {"31","pd1"},
+       {"28","pd4"},
+       {"29","pd3"},
+       {"42","pd6"},
+       {"43","pd5"},
+       {null,"GND"},
+       {"41","pd7"},
+       {"39","pd9"},
+       {"40","pd8"}
     };
     
 }
--- Cubieboard2-Java/src/gpio/SimpleGpio.java
+++ Cubieboard2-Java/src/gpio/SimpleGpio.java
@@ -10,21 +10,35 @@ package gpio;
  * @author Tiagop
  */
 public class SimpleGpio {
-    /**
-     * 
-     * array contendo informaçoes sobre os pinos
+    /** 
+     * Contante que guarda String que será usada para gerar comando RunTime(no caso, Leitura de dados em um pino gpio).
+     * Deve ser usada ao inserir parametros "direction", nos metodos dessa classe
      */
     public static final String INPUT = "in";
+    /**
+     * 
+     * Contante que guarda String que será usada para gerar comando RunTime(no caso, Escrita de dados em um pino gpio).
+     * Deve ser usada ao inserir parametros "direction", nos metodos dessa classe
+     */
     public static final String OUTPUT = "out";
     
+    /**
+     * Importando a classe que contém o mapeamento de pinos
+     */
     Pins pinos = new Pins();
-    
+    /**
+     * Usado para importar Pinos GPio na CB2, utilizando numero de referencia no Cubian OS
+     * 
+     * @param pin numero de referencia do pino a ser importado(relacionado as portas físicas no .fex file)
+     * @param direction INPUT ou OUTPUT
+     * @return 'false' se a operação não foi concluida com sucesso; 'true' se o pino foi importado corretamente
+     */
     public boolean importPin(int pin, String direction){
         int index = 0;
         boolean isFound = false;
-        while(index < pinos.pinsHDMI.length && !isFound){
-            System.out.println("pino "+pinos.pinsHDMI[index][0]);
-            if(String.valueOf(pin).equals(pinos.pinsHDMI[index][0]))
+        while(index < pinos.pinsUSB.length && !isFound){
+            System.out.println("pino "+pinos.pinsUSB[index][0]);
+            if(String.valueOf(pin).equals(pinos.pinsUSB[index][0]))
                 isFound = true;
             else
                 ++index;
@@ -35,6 +49,18 @@ public class SimpleGpio {
             return true;
         }
         else
+        return false;
+    }
+    /**
+     * Função não implementada
+     * 
+     * @param fisicalPin posição física do pino na CB2
+     * @param direction idem ao Método importPin()
+     * @return 'false'
+     */
+    public boolean importPinByFisical(int fisicalPin, String direction){
+        
+        System.out.println("NOT IMPLEMENTED");
         return false;
     }
     
